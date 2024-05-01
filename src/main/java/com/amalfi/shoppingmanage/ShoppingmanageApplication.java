ffe69aba63e2984a9a868d17137c3895366c1d90
napule77 <arturo.amalfi@gmail.com>
2024-05-01 16:27:43 +0200

    COMMIT INIZIALE

---
+++ src/main/java/com/amalfi/shoppingmanage/ShoppingmanageApplication.java
@@ -0,0 +1,13 @@
+package com.amalfi.shoppingmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingmanageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingmanageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amalfi/shoppingmanage/ShoppingmanageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amalfi.shoppingmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmanageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
